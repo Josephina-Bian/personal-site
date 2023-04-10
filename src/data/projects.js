8d9385ebf692120a38e37343f13b69d180520e80
Josephina-Bian <zhennibi@usc.edu>
2023-04-10 02:05:21 -0700

    update my project and contact

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,48 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
-    desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
-  },
-  {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
+    title: 'Analysis of the Newest Social Media Dataset Focused on “Clean” and Less Toxic Social Media: the Pixstory',
+    subtitle: 'Group Project: Data Science at Scale',
     link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2023-4-30',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      'A three-month project built for a DSCI course. '
+      + 'Examined the target market and user behaviors through '
+      + 'Correlation Analysis, Content Analysis, Sentiment Analysis, Hate Speech & Sarcasm Detection, Clustering Analysis, Image Analysis, Language Detection & Translation, Location Analysis, and Data Visulization. '
+      + 'Utilized multiple Python libraries, various NLP and ML algorithms and Docker containers.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
+    title: 'Biomedical Data Analysis by using K-Nearest Neighbors Algorithms',
+    subtitle: 'Individual Project: Machine Learning for Data Science',
+    link: 'https://devpost.com/software/harvest',
+    image: '/images/projects/scatterplot.png',
+    date: '2023-2-3',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'Built a KNN classifer predicting the prescence of spinal column pathologies based on the biomechanical features of the vertebrae using the Vertebral Column Data created by Dr. Henrique de Mota. '
+      + 'Six primary biomechanical features of the vertebrae were measured using X-rays of the spines. '
+      + 'Optimized the classifier by testing with differenct distance metrics and performing cross-validation for the parameter K.',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
+    title: 'Analysis of Los Angeles 1b1b Apartment Market',
+    subtitle: 'Individual Project: Principles of Programming for Data Science',
+    link: 'https://docs.google.com/presentation/d/1u3sZHh5jBeSefSuU7WIVctkQ4XYFgDCDxMTCC_Xbm0g/edit?usp=sharing',
+    image: '/images/projects/apartments.jpg',
+    date: '2022-12-08',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'Gathered real-time data through Python web scraping library BeautifulSoup, Google Review API and Google Geolocation API. '
+      + 'Conducted a correlation analysis between rent, online reviews and neighborhood. '
+      + 'Organized and modeled data into CSV files. Visualized data by using Python Matplotlib.',
+  },
+  {
+    title: 'Conceptual Design for Task-Management and Choice-Assistant App "Act Fast, Think Slow"',
+    subtitle: 'Individual Project: Theoretical Approaches to Communication Data Science',
+    link: 'https://docs.google.com/presentation/d/1JstfW0mEIAzqYcXf-49yalF4bUtM_m4NRrn2O4Hw3EY/edit?usp=sharing',
+    image: '/images/projects/app.jpg',
+    date: '2022-12-03',
+    desc:
+      'Wrote a comprehensive product requirement document for the mobile application detailing the target market, competitive analysis and execution plan. '
+      + 'Presented the conceptual design and product showcase publicly using the UI interface design tool Figma.',
   },
 ];
 
