a2312a0b5035cbaa2f61089340d879fed61b4819
Josephina-Bian <zhennibi@usc.edu>
2023-04-09 22:45:07 -0700

    update names

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Josephina Zhenni Bian via email @ zhennibi@usc.edu"
   >
     <article className="post" id="contact">
       <header>
